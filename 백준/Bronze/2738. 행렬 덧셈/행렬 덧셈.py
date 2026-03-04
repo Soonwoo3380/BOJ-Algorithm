4ad1349f1b6f8c47879e4d309b906f08bb8914c3
Soonwoo Kim <111051116+epdlxjwhdk258@users.noreply.github.com>
2026-03-04 21:53:25 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+A = []
+B = []
+
+for i in range(N):
+    a = list(map(int, input().split()))
+    A.append(a)
+for j in range(N):
+    b = list(map(int, input().split()))
+    B.append(b)
+    
+for k in range(N):
+    for l in range(M):
+        A[k][l] += B[k][l]
+    print(*A[k])    
