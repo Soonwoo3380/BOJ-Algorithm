5adcba30b9bff43bd3d52ae9b65784f03dfe2620
Soonwoo Kim <111051116+epdlxjwhdk258@users.noreply.github.com>
2026-03-04 21:56:27 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+people = list(range(1, N+1))
+
+result = []
+
+idx = 0
+
+while people:
+    idx = (idx + K - 1) % len(people)
+    result.append(str(people.pop(idx)))
+
+print(f"<{', '.join(result)}>")
